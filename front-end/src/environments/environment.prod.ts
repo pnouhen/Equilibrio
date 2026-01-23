e0c7d2f13bc10aa3801fbabf96ee2bf5bedf344e
Pierre Nouhen <pierre.nouhen@orange.fr>
2026-01-23 14:19:47 +0100

    feat: config for vercel 6

--- front-end/src/environments/environment.prod.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-  production: true,
-  LINK_URL: 'http://pierre-nouhen-equilibrio.vercel'
-};
--- front-end/src/environments/environment.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-  production: false,
-  LINK_URL: 'http://localhost:4200'
-};
